90821182fc16002f6bfa4fc5d49d4161d4fdb05f
denisp22 <denispetrov722@gmail.com>
2022-06-13 03:10:55 -0600

    Adding Product controller & model

---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  img         :string
+#  name        :string
+#  price       :float
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Product < ApplicationRecord
+end
